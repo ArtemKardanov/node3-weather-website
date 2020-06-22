2fc0f48b7c00482637acdaac235e2286f635cd23
Your Name <rtkard90@gmail.com>
2020-06-23 00:31:18 +0300

    add description

--- public/js/app.js
+++ public/js/app.js
@@ -3,12 +3,14 @@ console.log("Client side javascript is loaded!");
 const weatherForm = document.querySelector(".form");
 const search = document.querySelector(".input");
 const dataMessage = document.querySelector(".data");
+const dataMessage2 = document.querySelector(".data-2");
 
 weatherForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
   const location = search.value;
   dataMessage.textContent = "Loading...";
+  dataMessage2.textContent = "";
 
   fetch(`/weather?address=${location}`).then((response) =>
     response.json().then((data) => {
@@ -16,7 +18,8 @@ weatherForm.addEventListener("submit", (e) => {
         dataMessage.textContent = data.error;
         console.log(data.error);
       } else {
-        dataMessage.textContent = `Location: ${data.location}. ${data.forecast}`;
+        dataMessage.textContent = `Location: ${data.location}`;
+        dataMessage2.textContent = data.forecast;
 
         console.log(data.location);
         console.log(data.forecast);
